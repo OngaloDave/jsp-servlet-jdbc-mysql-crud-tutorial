d6a49949f0262c34b6d3018bd462221c292aa772
David Ongalo <117847150+OngaloDave@users.noreply.github.com>
2023-05-04 21:37:42 +0300

    Update create-user-table.sql

--- sql/create-user-table.sql
+++ sql/create-user-table.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE 'demo';
-USE demo;
+CREATE TABLE IF NOT EXISTS users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(120) NOT NULL,
+    email VARCHAR(220) NOT NULL,
+    country VARCHAR(120),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table users (
-	id  int(3) NOT NULL AUTO_INCREMENT,
-	name varchar(120) NOT NULL,
-	email varchar(220) NOT NULL,
-	country varchar(120),
-	PRIMARY KEY (id)
-);
 
